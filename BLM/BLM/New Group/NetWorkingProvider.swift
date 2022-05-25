4f3a4273ff29c657d75904935305d7f3fab69a18
Jose96Luis <88519400+Jose96Luis@users.noreply.github.com>
2022-05-24 23:01:47 -0500

    initialCommit

---
+++ BLM/BLM/New Group/NetWorkingProvider.swift
@@ -0,0 +1,17 @@
+//
+//  NetWorkingProvider.swift
+//  BLM
+//
+//  Created by Jose Luis Rodriguez on 24/05/22.
+//
+
+import Foundation
+import Alamofire
+
+final class NetworkingProvider {
+    static let shared = NetworkingProvider()
+
+    let urlBase = "https://gorest.co.in/public/v1/users"
+
+    
+}
---
+++ BLM/BLM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLM
+//
+//  Created by Jose Luis Rodriguez on 24/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
