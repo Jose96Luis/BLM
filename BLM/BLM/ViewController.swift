9e70477debd2a5ef72b6a07848a7c6dfc01cf23f
Jose Luis <jose_luis1996@live.com.mx>
2022-05-25 16:05:52 -0500

    AddClick

--- BLM/BLM/ViewController.swift
+++ BLM/BLM/ViewController.swift
@@ -10,11 +10,30 @@ import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    private let myMenu = ["Lector de codigos QR, Lista de productos"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        
     }
-
-
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myMenu.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell?.textLabel?.text = myMenu[indexPath.row]
+        
+        cell?.accessoryType = .none
+    }
+
+}
